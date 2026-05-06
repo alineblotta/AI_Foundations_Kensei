31a3514b39d717c86f95a09e63d5a25c94bd6924
alineblotta <aline_blotta@hotmail.com>
2026-05-06 19:55:48 -0300

    Organização das pastas

--- semana-02/Exercícios/ola.py
+++
@@ -1,6 +0,0 @@
-# Meu primeiro programa Python!
-print("Ola, Kensei!")
-print("Eu sou", "um futuro dev")
-# Perguntando o nome do usuario
-nome = input("Qual seu nome? ")
-print("Bem-vindo,", nome, "!")
